4271fda57d5569b4d0d2889260a8e2ff4c1bdcdd
rishmalkp <rishmalkpc7@gmail.com>
2023-07-28 21:47:15 +0400

    Built a simple word counter with django as backend using different request methods.

---
+++ myapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    context = {
+        'name' : 'Patrick',
+        'age' : 25,
+        'nationality' : 'British',
+    }
+    return render(request, 'index.html')
+
+def counter(request):
+    text = request.POST['text']
+    amount_of_words = len(text.split())
+    return render(request, 'counter.html', {'amount': amount_of_words})
